8a224f966cb5fe72b7ce26ab975aa4192596a108
Andy <andy.huang@wnc.com.tw>
2018-02-21 19:11:07 +0800

    modify hello to accept argument

--- externel/hello/hello.c
+++ externel/hello/hello.c
@@ -8,6 +8,17 @@ int main(int argc, char** argv)
 {
 	int fd = -1;
 	int val = 0;
+	int write_val = 0;
+	if (argc < 2 )
+	{
+		printf("Usage: hello write_value\n");
+		return -1 ;
+	}
+	else
+	{
+		write_val = atoi(argv[1]);
+	}
+	
 	fd = open(DEVICE_NAME, O_RDWR);
 	if(fd == -1) {
 		printf("Failed to open device %s.\n", DEVICE_NAME);
@@ -17,9 +28,10 @@ int main(int argc, char** argv)
 	printf("Read original value:\n");
 	read(fd, &val, sizeof(val));
 	printf("%d.\n\n", val);
-	val = 5;
-	printf("Write value %d to %s.\n\n", val, DEVICE_NAME);
-        write(fd, &val, sizeof(val));
+	
+	//val = 5;
+	printf("Write value %d to %s.\n\n", write_val, DEVICE_NAME);
+        write(fd, &write_val, sizeof(write_val));
 	
 	printf("Read the value again:\n");
         read(fd, &val, sizeof(val));
